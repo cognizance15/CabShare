724d1473ed582ba5918dff153abc155c628274dc
Safiyat Reza <md.reza@snapdeal.com>
2015-08-13 16:49:14 +0530

    Driver in progress.

--- src/com/cabshare/dao/DriverDao.java
+++ src/com/cabshare/dao/DriverDao.java
@@ -6,5 +6,5 @@ import com.cabshare.entity.User;
 public interface DriverDao extends UserDao{
 	boolean beginRide(User user, Driver driver);
 	boolean endRide(User user, Driver driver);
-	boolean nextPosition(String position, Driver driver);
+	boolean nextPosition(int position, Driver driver);
 }
