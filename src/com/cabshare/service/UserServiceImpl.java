81ab446e4c42f1176649599022ac0e103acdaf46
imthegod <pardhasaradhi108@gmail.com>
2015-08-13 12:30:27 +0530

    Added service Structure

---
+++ src/com/cabshare/service/UserServiceImpl.java
@@ -0,0 +1,37 @@
+package com.cabshare.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import com.cabshare.dao.UserDao;
+import com.cabshare.entity.User;
+
+@Service(value="userService")
+public class UserServiceImpl implements UserService{
+
+	@Autowired
+	@Qualifier(value="userDao")
+	UserDao userDao;
+	
+	@Override
+	public boolean register(User user) {
+		return userDao.register(user);
+	}
+
+	@Override
+	public boolean login(String uname, String password) {
+		return userDao.login(uname, password);
+	}
+
+	@Override
+	public boolean changePassword(String username, String password) {
+		return userDao.changePassword(username, password);
+	}
+
+	@Override
+	public boolean updateDetails(User user) {
+		return userDao.updateDetails(user);
+	}
+
+}
